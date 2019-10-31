b38a081c0cf5507d411013c4f46cc64fd0c6b163
khalsaniwas <honeysaini21@gmail.com>
2019-10-31 16:03:02 -0400

    Add success and failure message

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,23 +1,10 @@
 'use strict'
+
 const store = require('../store')
 const successMessage = function (newText) {
-  $('#message').text(newText)
-  $('#message').removeClass('alert-warning')
-  $('#message').addClass('alert-success')
+  $('#user_message').text(newText)
 }
 
 const failureMessage = function (newText) {
-  $('#message').text(newText)
-  $('#message').removeClass('alert-success')
-  $('#message').addClass('alert-warning')
-}
-
-const onSignUpSuccess = function () {
-  successMessage('Signed up successfully')
-  $('#sign-up').trigger('reset')
-}
-
-const onSignUpFailure = function () {
-  failureMessage('Sign up failed')
-  $('#sign-up').trigger('reset')
+  $('#user_message').text(newText)
 }
