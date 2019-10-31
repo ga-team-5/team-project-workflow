034c2a80cf5419347c14e4608c8d396aa403da06
khalsaniwas <honeysaini21@gmail.com>
2019-10-31 15:57:35 -0400

    Update ui

---
+++ assets/scripts/ui.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const store = require('../store')
+const successMessage = function (newText) {
+  $('#user_message').text(newText)
+
+}
+
+const failureMessage = function (newText) {
+  $('#user_message').text(newText)
+
+}
